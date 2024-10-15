796936a9a3cff030457efb66a46e689fa0778df9
ali <alitalal096@gmail.com>
2024-10-15 13:05:40 +0300

    init

---
+++ eCommerceWebsite/products/views.py
@@ -0,0 +1,14 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import Product
+
+def home(request):
+    return render(request,'products/home.html')
+
+def products(request):
+    products = Product.objects.all()
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+    context = {'products': paged_products}
+    return render(request,'products/products.html', context)
